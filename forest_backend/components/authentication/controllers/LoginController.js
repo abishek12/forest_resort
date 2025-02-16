ce28ff7178c76697edda0b61d55d0b36123da7d4
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-02-16 17:06:16 +0545

    Add refresh token functionality with endpoint and validation

--- forest_backend/components/authentication/controllers/LoginController.js
+++ forest_backend/components/authentication/controllers/LoginController.js
@@ -60,3 +60,48 @@ export const loginUser = async (req, res) => {
     });
   }
 };
+
+/**
+ *
+ * @param {token} req
+ * @route POST /api/auth/refresh-token
+ * @access Public
+ * @returns
+ */
+
+export const refreshToken = async (req, res) => {
+  try {
+    let { token: refreshToken } = req.body;
+
+    if (!refreshToken) {
+      return res.status(403).json({
+        message: "Refresh Token Required",
+      });
+    }
+
+    // verify the jwt token
+    let decoded = null;
+    try {
+      decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH);
+    } catch (err) {
+      return res.status(403).json({ message: "Invalid Token" });
+    }
+
+    let user = await User.findById(decoded.userId);
+    if (!user || user.refreshToken !== refreshToken) {
+      return res.status(403).json({ message: "Invalid Refresh Token" });
+    }
+
+    let accessToken = await tokenGenerator(user);
+
+    return res.status(200).json({
+      message: "success",
+      token: accessToken,
+    });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    return res.status(500).json({
+      message: `Error: ${error}`,
+    });
+  }
+};
--- forest_backend/components/authentication/router/AuthRoute.js
+++ forest_backend/components/authentication/router/AuthRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { registerUser } from "../controllers/RegisterController.js";
-import { loginUser } from "../controllers/LoginController.js";
+import { loginUser, refreshToken } from "../controllers/LoginController.js";
 
 const route = express.Router();
 
-route.post("/register", registerUser);
-route.post("/login", loginUser);
+route
+  .post("/register", registerUser)
+  .post("/login", loginUser)
+  .post("/refresh-token", refreshToken);
 
 export default route;
