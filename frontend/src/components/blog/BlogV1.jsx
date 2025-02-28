5a985429f499c8f1071f1616a07450a361721375
Milan Rai <milanraiyt07@gmail.com>
2025-02-28 11:54:25 +0545

    issue fixed again

--- frontend/src/components/blog/BlogV1.jsx
+++ frontend/src/components/blog/BlogV1.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 import BlogV1Data from "../../jsonData/BlogV1Data.json";
 import SingleBlogV1 from "./SingleBlogV1";
 import { SingleBlog3d } from "./SingleBlog3d";
@@ -8,7 +8,6 @@ import { fadeInCards } from "../../utils/fadeInAnimation";
 import { Highlight } from "../ui/aceternity_ui/text-highlight";
 import { useSelector } from "react-redux";
 
-<<<<<<< HEAD
 const staticBlog = [
   {
     id: '0',
@@ -31,18 +30,9 @@ const staticBlog = [
     date: '25 April, 2024',
     title: '⚽ Experience the Thrill of an Exciting Futsal Match Together! 🏅',
   },
+];
 
-]
-=======
->>>>>>> 1b623ca2972a84a66c8d67f2e26d4e2af8aeff60
 const BlogV1 = () => {
-  const blogList = useSelector((state) => state.blogList);
-  const { blogs } = blogList;
-
-  const sortedBlogs = blogs
-    ?.slice()
-    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   return (
     <>
       <div className="blog-area home-blog default-padding bottom-less">
@@ -53,38 +43,23 @@ const BlogV1 = () => {
                 <h5 className="sub-title">
                   <BoxReveal>News & Events</BoxReveal>
                 </h5>
-<<<<<<< HEAD
-                <div class="common-header">  {/**new styles */}
+                <div className="common-header">
+                  {/* Keep your header */}
                   Check Out Our Blog Posts
                 </div>
-=======
-                <h3 class="tw-text-2xl md:tw-text-5xl">
-                  <Highlight>Read Our Latest Posts</Highlight>
-                </h3>
->>>>>>> 1b623ca2972a84a66c8d67f2e26d4e2af8aeff60
               </div>
             </div>
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="container ">
+        <div className="container">
           <div className="tw-flex tw-justify-center tw-gap-4">
             {staticBlog.slice(0, 3).map((item, index) => (
-=======
-        <div className="container">
-          <div className="row">
-            {sortedBlogs.slice(0, 3).map((blog) => (
->>>>>>> 1b623ca2972a84a66c8d67f2e26d4e2af8aeff60
               <motion.div
                 variants={fadeInCards}
                 initial="initial"
                 whileInView="animate"
-<<<<<<< HEAD
                 key={index}
-              // className="col-xl-4 col-md-6 mb-30"
-
               >
-                {/* <SingleBlogV1 blog={blog} /> */}
                 <SingleBlog3d
                   image={item.img}
                   author={item.author}
@@ -94,26 +69,6 @@ const BlogV1 = () => {
                 />
               </motion.div>
             ))}
-            {/* {(!sortedBlogs || sortedBlogs.length === 0) && (
-              <div className="tw-text-center tw-text-gray-500 tw-text-lg tw-p-4 tw-border tw-border-dashed tw-border-gray-300 tw-rounded-md">
-                No blogs available at the moment...
-              </div>
-            )} */}
-=======
-                custom={blog._id}
-                className="col-xl-4 col-md-6 mb-30"
-                key={blog._id}
-              >
-                {/* <SingleBlogV1 blog={blog} /> */}
-                <SingleBlog3d blog={blog} />
-              </motion.div>
-            ))}
-            {(!sortedBlogs || sortedBlogs.length === 0) && (
-              <div className="tw-text-center tw-text-gray-500 tw-text-lg tw-p-4 tw-border tw-border-dashed tw-border-gray-300 tw-rounded-md">
-                No blogs available at the moment...
-              </div>
-            )}
->>>>>>> 1b623ca2972a84a66c8d67f2e26d4e2af8aeff60
           </div>
         </div>
       </div>
--- frontend/src/components/blog/SingleBlog3d.jsx
+++ frontend/src/components/blog/SingleBlog3d.jsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-
 import Arrow from '../../assets/icons/Arrow.svg';
 
 export function SingleBlog3d({ id, image, title, date, author }) {
-
   return (
     <div className="tw-bg-yellow-400 tw-h-auto tw-w-auto tw-shadow-lg tw-border-[3px] tw-border-white tw-overflow-hidden"
       style={{ backgroundColor: 'rgba(181, 222, 76, 0.12)' }}
@@ -20,7 +17,7 @@ export function SingleBlog3d({ id, image, title, date, author }) {
           borderBottom: 'white'
         }}
       >
-        <div className='' >
+        <div>
           <p className="tw-font-bold tw-text-[15px] tw-leading-[10px] tw-tracking-[0.05em] tw-opacity-85">{author}</p>
           <p className="tw-font-semibold tw-text-[12px] tw-leading-[10px] tw-tracking-[0.05em] tw-opacity-70">{date}</p>
         </div>
@@ -35,93 +32,3 @@ export function SingleBlog3d({ id, image, title, date, author }) {
     </div>
   );
 }
-
-// {/* <CardContainer className="pb-2 tw-rounded-md tw-bg-[#def7f7] tw-bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
-//   <CardBody className="relative">
-//     <CardItem translateZ="100">
-//       {/* <img
-//             loading="lazy"
-//             src={`/img/blog/${thumb}`}
-//             className="tw-rounded-t-lg"
-//             alt="thumbnail"
-//           /> */}
-//       <ImageGrid images={images} />
-//       <div className="tags">
-//         <Link to={void 0}>{category}</Link>
-//       </div>
-//     </CardItem>
-//     <div className="tw-px-2 tw-pt-4">
-//       <CardItem translateZ="80">
-//         <ul>
-//           <li>
-//             <Link to={void 0}>{author}</Link>
-//           </li>
-//           <li>{dateTimeFormat(createdAt)}</li>
-//         </ul>
-//       </CardItem>
-//       <CardItem as="h3" translateZ="60" className="post-title">
-//         <Link to={`/blog-single/${_id}`}>{title}</Link>
-//       </CardItem>
-//       <CardItem
-//         translateZ="60"
-//         as={Link}
-//         to={`/blog-single/${_id}#`}
-//         target="__blank"
-//         className="text-neutral-500 text-sm max-w-sm post-title tw-flex tw-items-center tw-gap-2"
-//       >
-//         Continue Reading <FaArrowRight />
-//       </CardItem>
-//     </div>
-//   </CardBody>
-// </CardContainer> */}
-=======
-import { CardBody, CardContainer, CardItem } from "../ui/aceternity_ui/3dcard";
-import { HashLink as Link } from "react-router-hash-link";
-import ImageGrid from "./ImageGrid";
-import { dateTimeFormat } from "../../utils/date-time";
-import { FaArrowRight } from "react-icons/fa";
-
-export function SingleBlog3d({ blog }) {
-  const { _id, images, category, author, createdAt, title } = blog;
-  return (
-    <CardContainer className="pb-2 tw-rounded-md tw-bg-[#def7f7] tw-bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
-      <CardBody className="relative">
-        <CardItem translateZ="100">
-          {/* <img
-            loading="lazy"
-            src={`/img/blog/${thumb}`}
-            className="tw-rounded-t-lg"
-            alt="thumbnail"
-          /> */}
-          <ImageGrid images={images} />
-          <div className="tags">
-            <Link to={void 0}>{category}</Link>
-          </div>
-        </CardItem>
-        <div className="tw-px-2 tw-pt-4">
-          <CardItem translateZ="80">
-            <ul>
-              <li>
-                <Link to={void 0}>{author}</Link>
-              </li>
-              <li>{dateTimeFormat(createdAt)}</li>
-            </ul>
-          </CardItem>
-          <CardItem as="h3" translateZ="60" className="post-title">
-            <Link to={`/blog-single/${_id}`}>{title}</Link>
-          </CardItem>
-          <CardItem
-            translateZ="60"
-            as={Link}
-            to={`/blog-single/${_id}#`}
-            target="__blank"
-            className="text-neutral-500 text-sm max-w-sm post-title tw-flex tw-items-center tw-gap-2"
-          >
-            Continue Reading <FaArrowRight />
-          </CardItem>
-        </div>
-      </CardBody>
-    </CardContainer>
-  );
-}
->>>>>>> 1b623ca2972a84a66c8d67f2e26d4e2af8aeff60
